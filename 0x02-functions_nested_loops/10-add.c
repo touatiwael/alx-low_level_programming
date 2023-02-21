d6fd22dbad099fafbbae0500c97416c939b3d66f
wael touati <waeltouati@live.com>
2023-02-21 13:29:39 -0800

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -14,35 +14,3 @@ int add(int a, int b)
 	return (a + b);
 }
 
-Task 11
-
-vi 11-print_to_98.c
-
-#include <stdio.h>
-/**
- * print_to_98 - Prints all natural numbers
- * from n to 98.
- *
- * @n: input number.
- *
- * Return: no return.
- */
-void print_to_98(int n)
-{
-	if (n > 98)
-	{
-		for (; n > 98; n--)
-		{
-			printf("%d, ", n);
-		}
-	}
-	else if (n < 98)
-	{
-		for (; n < 98; n++)
-		{
-			printf("%d, ", n);
-		}
-	}
-	printf("%d\n", n);
-}
-
